7501548478a4892df1ce0772e8b5f5555342808e
Alpet Gexha <agexha@gmail.com>
2022-04-25 04:02:00 +0200

    Add Application Health

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 

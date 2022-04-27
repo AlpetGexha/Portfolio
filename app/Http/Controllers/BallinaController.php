1b65411740aaa5b7edeebd7cce3129678117831e
Alpet Gexha <agexha@gmail.com>
2022-04-28 01:00:22 +0200

    Update BallinaController.php

--- app/Http/Controllers/BallinaController.php
+++ app/Http/Controllers/BallinaController.php
@@ -10,10 +10,17 @@ class BallinaController extends Controller
 {
     public function index()
     {
-        $about = Aboutme::select('name', 'profile', 'email', 'phone', 'body')->first()->get();
-        $skills = Aboutme::select('skills')->first()->pluck('skills')->toJson();
+
+        $about = Aboutme::select('name', 'profile', 'email', 'phone', 'body')->first()->get(); // x
+        $skills = Aboutme::select('skills')->first()->pluck('skills')->toJson(); // x
         // $skills = Response::json($skillss);;
-        $skills = json_decode($skills);
-        return view('dashboard', compact('about', 'skills'));
+        $skills = json_decode($skills);  // x
+        // map services
+        $services = Aboutme::select('services')->first()->pluck('services')->toJson(); //
+        $services = json_decode($services);  // x
+
+        $facts = Aboutme::select('facts')->first()->pluck('facts')->toJson(); // x
+        $facts = json_decode($facts);
+        return view('dashboard', compact('about', 'skills', 'services','facts'));
     }
 }

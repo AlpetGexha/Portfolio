911fa33486462251bcb103ee5611dc4afc5ded4c
Alpet Gexha <agexha@gmail.com>
2022-04-30 00:39:57 +0200

    Add 3 Marco

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+
+        /**
+         * Read time of the post 200word = 1min
+         */
+        Str::macro('readTime', function (...$text) {
+            $totalWords = str_word_count(implode(" ", $text));
+            $minutesToRead = round($totalWords / 200);
+
+            return (int)max(1, $minutesToRead);
+        });
+
+        Str::macro('removehtml', function ($text) {
+            return preg_replace('/(\n){1,}/m', '<br />', stripcslashes(strip_tags($text)));
+        });
+
+        Str::macro('realReadTime', function ($text) {
+            return Str::readTime(Str::removehtml($text));
+        });
     }
 }

4a32b54e33ee2dc0cc7f00b0308dfd52723850c5
Alpet Gexha <agexha@gmail.com>
2022-05-15 17:38:48 +0200

    Add Views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,17 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Posts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class PostController extends Controller
 {
-    public function single(Posts $post)
+    public function single(Posts $post, Request $r)
     {
+        if (RateLimiter::remaining($r->ip(), $perMinute = 1)) {
+            RateLimiter::hit($r->ip());
+            // dd($r->ip());
+            $post->update(['views' => $post->views + 1]);
+        }
+
         return view('post.single', compact('post'));
     }
 
     public function index()
     {
-       $posts = Posts::paginate(12);
+        $posts = Posts::with(['user', 'media'])->where('status', 1)->paginate(12);
         return view('post.show', compact('posts'));
     }
 }
